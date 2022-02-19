622aff47d747fc65af174283d3648625d5641612
Sameer <hmssameer55@gmail.com>
2022-02-19 11:40:48 +0530

    corrections

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ passport.deserializeUser((user, done)=> {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://guarded-headland-22735.herokuapp.com/auth/google/secrets"
 },
     function (accessToken, refreshToken, profile, cb) {
         userModel.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -48,7 +48,6 @@ passport.use(new GoogleStrategy({
         });
     }
 ));
-
 
 
 app.get('/', (req, res) => {
